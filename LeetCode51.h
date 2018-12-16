598ed251d14daa926ae6f6d86907d89436dbb3dc
huangx916 <huangx916@163.com>
2018-12-16 13:54:36 +0800

    algorithm training

---
+++ LeetCode51.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+// N皇后问题，使用DSF及回溯法
+class Solution51
+{
+public:
+	vector<vector<string>> solveNQueens(int n) {
+		vector<vector<string>> ret;
+		int total = 0;
+		vector<int> state;
+		DFS(0, n, state, total, ret);
+		return ret;
+	}
+
+	void DFS(int row, int n, vector<int>& state, int& total, vector<vector<string>>& ret)
+	{
+		if (row == n)
+		{
+			total += 1;
+			convert(state, ret, n);
+			return;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			if (isValid(row, i, state))
+			{
+				state.push_back(i);
+				DFS(row + 1, n, state, total, ret);
+				// 回溯法
+				state.pop_back();
+			}
+		}
+	}
+
+	void convert(vector<int>& state, vector<vector<string>>& ret, int n)
+	{
+		vector<string> vct;
+		for (int i = 0; i < state.size(); ++i)
+		{
+			string str(n, '.');
+			str[state[i]] = 'Q';
+			vct.push_back(str);
+		}
+		ret.push_back(vct);
+	}
+
+	bool isValid(int row, int col, vector<int>& state)
+	{
+		for (int i = 0; i < state.size(); ++i)
+		{
+			if (state[i] == col || abs(row - i) == abs(col - state[i]))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "stdlib.h"
+#include <iostream>
+#include "LeetCode51.h"
+#include "LeetCode52.h"
+
+using namespace std;
+
+int main()
+{
+
+
+	/*Solution51 sln51;
+	sln51.solveNQueens(4);*/
+
+	/*Solution52 sln52;
+	int ret = sln52.totalNQueens(4);
+	std::cout << ret << std::endl;*/
+
+	system("pause");
+	return 0;
+}
