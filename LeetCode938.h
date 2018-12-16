da0af961aff3291c6cc6f1b30744cf4747cd100b
huangx916 <huangx916@163.com>
2018-12-16 23:36:28 +0800

    leetcode 938

---
+++ LeetCode938.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#include <stdlib.h>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// 二叉搜索树使用中序遍历、深度优先(用递归简单，用stack中序后序比较复杂)
+class Solution938
+{
+public:
+	int rangeSumBST(TreeNode* root, int L, int R) {
+		int ret = 0;
+		bool start = false;
+		vector<int> vct;
+		inOrderTraverse(root, vct);
+		for (int i = 0; i < vct.size(); ++i)
+		{
+			if (vct[i] == L)
+			{
+				ret += vct[i];
+				start = true;
+			}
+			else if (vct[i] == R)
+			{
+				ret += vct[i];
+				break;
+			}
+			else if(start)
+			{
+				ret += vct[i];
+			}
+		}
+		return ret;
+	}
+
+	void inOrderTraverse(TreeNode* node, vector<int>& vct)
+	{
+		if (node->left)
+		{
+			inOrderTraverse(node->left, vct);
+		}
+		vct.push_back(node->val);
+		if (node->right)
+		{
+			inOrderTraverse(node->right, vct);
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -6,13 +6,12 @@
 #include "LeetCode228.h"
 #include "LeetCode343.h"
 #include "LeetCode482.h"
+#include "LeetCode938.h"
 
 using namespace std;
 
 int main()
 {
-
-
 	/*Solution51 sln51;
 	sln51.solveNQueens(4);*/
 
@@ -32,7 +31,21 @@ int main()
 	string ret = sln482.licenseKeyFormatting("t5F3Z-2e-9-w", 4);
 	cout << ret << endl;*/
 
-	
+	Solution938 sln938;
+	TreeNode n1(10);
+	TreeNode n2(5);
+	TreeNode n3(15);
+	TreeNode n4(3);
+	TreeNode n5(7);
+	TreeNode n6(18);
+	n1.left = &n2;
+	n1.right = &n3;
+	n2.left = &n4;
+	n2.right = &n5;
+	n3.right = &n6;
+	int ret = sln938.rangeSumBST(&n1, 7, 15);
+	cout << ret << endl;
+
 
 	system("pause");
 	return 0;
